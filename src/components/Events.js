78dd56ade0f0c92e9ede1696a8399cc2f8e72889
jocelymarques <fulanodetal@exemplo.br>
2022-06-19 10:50:05 -0300

    envio de parâmentros

--- src/components/Events.js
+++ src/components/Events.js
@@ -4,11 +4,23 @@ const Events = () => {
         console.log(e);
     }
 
+    const renderSomething = (x) =>{
+        if(x){
+            return <h1>Parâmetro é Verdadeiro {x}</h1>
+        }else{
+            return <h1>Parâmetro é Falso {x}</h1>
+        }
+    }
+
 
     return (
         <div>
             <div>
                 <button onClick={handleMyEvents}>Click aqui!</button>
+            </div>
+            <div>
+                {renderSomething(true)}
+                {renderSomething(false)}
             </div>
         </div>
     )
